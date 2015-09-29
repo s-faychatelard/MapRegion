b08caf497353d5661d967c0ac888bcd405ebe07b
Sylvain FAY-CHATELARD <sylvain@dviance.net>
2015-09-29 16:58:44 +0200

    First commit

---
+++ ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  MapRegion
+//
+//  Created by Sylvain Fay-Chatelard on 9/29/15.
+//  Copyright © 2015 Sylvain Fay-Chatelard. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController, MKMapViewDelegate {
+    
+    @IBOutlet var mapView: MKMapView!
+    @IBOutlet var regionLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.regionLabel.text = ""
+    }
+    
+    func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
+        self.regionLabel.text = "CLLocationCoordinate2DMake(\(self.mapView.region.center.latitude), \(self.mapView.region.center.longitude))\n"
+                              + "MKCoordinateSpanMake(\(self.mapView.region.span.latitudeDelta), \(self.mapView.region.span.longitudeDelta));";
+    }
+    
+    @IBAction func copyToPasteboard() {
+        
+        UIPasteboard.generalPasteboard().string = self.regionLabel.text
+    }
+}
+
